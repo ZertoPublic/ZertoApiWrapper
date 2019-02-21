6800c0c24b6f6fa1194391156ea3d31c19bdf0e5
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-02-20 22:14:40 -0500

    Create Uninstall-ZertoVra.ps1

---
+++ ZertoApiWrapper/Public/Uninstall-ZertoVra.ps1
@@ -0,0 +1,24 @@
+function Uninstall-ZertoVra {
+    [cmdletbinding()]
+    param(
+        [Parameter( Mandatory = $true )]
+        [string[]]$hostName
+    )
+
+    begin {
+        $baseUri = "vras"
+    }
+
+    process {
+        foreach ($name in $hostName) {
+            $vraName = "Z-VRA-{0}" -f $name
+            $vraIdentifier = get-zertovra -vraName | Select-Object vraIdentifier -ExpandProperty vraIdentifier
+            $uri = "{0}/{1}" -f $baseUri, $vraIdentifier.toString()
+            Invoke-ZertoRestRequest -uri $uri -method "DELETE"
+        }
+    }
+
+    end {
+
+    }
+}
