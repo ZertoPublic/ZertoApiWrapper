ddc940f7a6d6c47b21fa185fd57d238afda37b1c
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-02-24 20:35:38 -0500

    Create Set-ZertoLicense and Tests

---
+++ ZertoApiWrapper/Public/Set-ZertoLicense.ps1
@@ -0,0 +1,25 @@
+function Set-ZertoLicense {
+    [cmdletbinding( SupportsShouldProcess = $true )]
+    param (
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = "License Key to apply to the Zerto Virtual Manager"
+        )]
+        [string]$licenseKey
+    )
+
+    begin {
+        $baseUri = "license"
+        $body = @{"LicenseKey" = $licenseKey}
+    }
+
+    process {
+        if ($PSCmdlet.ShouldProcess()) {
+            Invoke-ZertoRestRequest -uri $baseUri -body $($body | ConvertTo-Json) -method "PUT"
+        }
+    }
+
+    end {
+        # Nothing to do
+    }
+}
