6f3f9e0f496d304a5f81ba3eec86865db61f5cfe
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-02-26 13:05:46 -0500

    Create stop vpg clone operation.

---
+++ ZertoApiWrapper/Public/Stop-ZertoCloneVpg.ps1
@@ -0,0 +1,24 @@
+function Stop-ZertoCloneVpg {
+    param(
+        [Parameter(
+            HelpMessage = "Name of the VPG to stop cloning",
+            Mandatory = $true
+        )]
+        [string]$vpgName
+    )
+
+    begin {
+        $baseUri = "vpgs"
+        $vpgIdentifier = $(Get-ZertoVpg -name $vpgName).vpgIdentifier
+    }
+
+    process {
+        $uri = "{0}/{1}/CloneAbort" -f $baseUri, $vpgIdentifier
+        invoke-ZertoRestRequest -uri $uri -method "POST"
+    }
+
+    end {
+        # Nothing to do
+    }
+}
+
