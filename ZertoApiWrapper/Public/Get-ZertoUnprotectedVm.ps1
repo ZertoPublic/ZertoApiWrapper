e314e2a947e42ca13412e2ef773db9c84c6034f0
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-03-10 22:12:32 -0400

    Update to use locally set variable to avoid additional API Call

--- ZertoApiWrapper/Public/Get-ZertoUnprotectedVm.ps1
+++ ZertoApiWrapper/Public/Get-ZertoUnprotectedVm.ps1
@@ -1,6 +1,6 @@
 <# .ExternalHelp ./en-us/ZertoApiWrapper-help.xml #>
 function Get-ZertoUnprotectedVm {
     [cmdletbinding()]
-    $uri = "virtualizationsites/{0}/vms" -f $(Get-ZertoLocalSite).siteidentifier
+    $uri = "virtualizationsites/{0}/vms" -f $script:zvmLocalInfo.siteidentifier
     Invoke-ZertoRestRequest -uri $uri
 }
