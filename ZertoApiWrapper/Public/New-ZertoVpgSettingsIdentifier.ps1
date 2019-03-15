59f83cbb8a10cff0b4e8cacffc6e52cc0e9c8027
Wes Carroll <wcarroll@users.noreply.github.com>
2019-03-14 20:29:13 -0400

    Create switch when creating new VPG Settings

--- ZertoApiWrapper/Public/New-ZertoVpgSettingsIdentifier.ps1
+++ ZertoApiWrapper/Public/New-ZertoVpgSettingsIdentifier.ps1
@@ -2,17 +2,31 @@ function New-ZertoVpgSettingsIdentifier {
     [cmdletbinding()]
     param(
         [Parameter(
-            HelpMessage = "Identifier of the VPG to create a VPG settings identifier. If a vpgIdentifier is not provided, a new VPG settings object is created without any configured settings. This would be used for creating a new VPG from scratch."
+            HelpMessage = "Identifier of the VPG to create a VPG settings identifier. If a vpgIdentifier is not provided, a new VPG settings object is created without any configured settings. This would be used for creating a new VPG from scratch.",
+            ParameterSetName = "existingVpg",
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
         )]
-        [string]$vpgIdentifier = $null
+        [string]$vpgIdentifier,
+        [Parameter(
+            HelpMessage = "Use this switch when creating a vpgSettingsIdentifier for a new VPG",
+            ParameterSetName = "newVpg",
+            Mandatory = $true
+        )]
+        [switch]$newVpg
     )
 
     begin {
         $baseUri = "vpgSettings"
-        if ($null -eq $vpgIdentifier) {
-            $body = "{}"
-        } else {
-            $body = "{""VpgIdentifier"":""$vpgIdentifier""}"
+        switch ($PSCmdlet.ParameterSetName) {
+            "newVpg" {
+                $body = "{}"
+            }
+
+            "existingVpg" {
+                $body = "{""VpgIdentifier"":""$vpgIdentifier""}"
+            }
         }
     }
 
