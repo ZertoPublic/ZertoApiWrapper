711454c78e9a626a09b1607e64f93971c0bc9a86
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-03-18 10:06:10 -0400

    Fix HelpMessage Typo

--- ZertoApiWrapper/Public/Export-ZertoVpg.ps1
+++ ZertoApiWrapper/Public/Export-ZertoVpg.ps1
@@ -30,7 +30,7 @@ function Export-ZertoVpg {
         foreach ($name in $vpgName) {
             $vpgSettingsIdentifier = New-ZertoVpgSettingsIdentifier -vpgIdentifier $(Get-ZertoVpg -name $name).vpgIdentifier
             $vpgSettings = Get-ZertoVpgSetting -vpgSettingsIdentifier $vpgSettingsIdentifier
-            $filePath = "{0}\{1}.json" -f $outputPath, $name
+            $filePath = "{0}\{1}.json" -f $outputFolder, $name
             $vpgSettings | Convertto-Json -depth 10 | Out-File -FilePath $filePath
         }
     }
