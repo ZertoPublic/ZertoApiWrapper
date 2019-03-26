bc0f509870a21417a9d88d6961bf463169fa2fc2
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-03-26 19:22:51 -0400

    Consolidate Tests to One File

--- Tests/Public/ZertoApiWrapper.Tests.ps1
+++ Tests/Public/ZertoApiWrapper.Tests.ps1
@@ -1,9 +1,8 @@
+$moduleName = "ZertoApiWrapper"
 $moduleFileName = "ZertoApiWrapper.psm1"
 $filePath = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace 'Tests', 'ZertoApiWrapper'
 $fileName = (Split-Path -Leaf $MyInvocation.MyCommand.Path ) -replace '.Tests.', '.'
 $modulePath = $filePath -replace "Public", ""
-Import-Module $modulePath\$moduleFileName -Force
-
 $userName = "zerto\build"
 $password = ConvertTo-SecureString -String "ZertoBuild" -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential($userName, $password)
@@ -12,8 +11,12 @@ $credential = New-Object -TypeName System.Management.Automation.PSCredential($us
 $zertoServer = "192.168.1.100"
 $zertoPort = "7669"
 
+Import-Module $modulePath\$moduleFileName
+
 Describe "File Tests" {
-    $commands = Get-Command -Module ZertoApiWrapper | Select-Object -ExpandProperty Name
+    Remove-Module $moduleName -Force
+    Import-Module $modulePath\$moduleFileName
+    $commands = Get-Command -Module $moduleName | Select-Object -ExpandProperty Name
     foreach ($command in $commands) {
         $path = "{0}/{1}.ps1" -f $filePath, $command
         it "$command is backed by a file with the same name" {
@@ -21,3 +24,14 @@ Describe "File Tests" {
         }
     }
 }
+
+describe "External Help Defination Present" {
+    $publicFiles = Get-ChildItem "$filePath" -File
+    foreach ($file in $publicFiles) {
+        it "External Help File is Defined" {
+            Get-Content -Path $file.fullName -First 1 | should be "<# .ExternalHelp ./en-us/ZertoApiWrapper-help.xml #>"
+        }
+    }
+}
+
+Describe "Connection Tests"
