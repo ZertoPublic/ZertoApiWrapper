23bc5dbe99c0ef5f40d1cddd480d31badad4d47b
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-04-29 16:15:42 -0400

    Update For Pester Tests

--- ZertoApiWrapper/Public/Invoke-ZertoFailoverCommit.ps1
+++ ZertoApiWrapper/Public/Invoke-ZertoFailoverCommit.ps1
@@ -1,6 +1,6 @@
 <# .ExternalHelp ./en-us/ZertoApiWrapper-help.xml #>
 function Invoke-ZertoFailoverCommit {
-    [cmdletbinding()]
+    [cmdletbinding( SupportsShouldProcess = $true )]
     param(
         [Parameter(
             HelpMessage = "Name(s) of the VPG(s) to commit.",
@@ -16,21 +16,23 @@ function Invoke-ZertoFailoverCommit {
 
     begin {
         $baseUri = "vpgs"
-        if ( $reverseProtection ) {
-            $body = @{"IsReverseProtection" = $true}
-        } else {
-            $body = @{"IsReverseProtection" = $false}
-        }
     }
 
     process {
+        if ( $reverseProtection.IsPresent ) {
+            $body = @{"IsReverseProtection" = $true }
+        } else {
+            $body = @{"IsReverseProtection" = $false }
+        }
         foreach ($name in $vpgName) {
             $vpgId = $(Get-ZertoVpg -name $name).vpgIdentifier
             if ( -not $vpgId ) {
                 Write-Error "VPG: $name could not be found. Please check the name and try again. Skipping."
             } else {
                 $uri = "{0}/{1}/FailoverCommit" -f $baseUri, $vpgId
-                Invoke-ZertoRestRequest -uri $uri -body $($body | convertto-json) -method "POST"
+                if ($PSCmdlet.ShouldProcess($body, $uri)) {
+                    Invoke-ZertoRestRequest -uri $uri -body $($body | convertto-json) -method "POST"
+                }
             }
         }
     }
