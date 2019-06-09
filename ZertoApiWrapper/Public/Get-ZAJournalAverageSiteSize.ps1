eb528e3432a446c437061f2f87c2b209a9c478a9
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-06-09 14:58:04 -0400

    Create Function and Required Docs
    Get-ZAJournalAverageSize

---
+++ ZertoApiWrapper/Public/Get-ZAJournalAverageSiteSize.ps1
@@ -0,0 +1,31 @@
+<# .ExternalHelp ./en-us/ZertoApiWrapper-help.xml #>
+function Get-ZAJournalAverageSiteSize {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            HelpMessage = "The identifier of the recovery site. A site identification is required for at least one site.",
+            Mandatory
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$recoverySiteIdentifier,
+        [Parameter(
+            HelpMessage = "The starting date of the report, in RFC 3339 standard ('1970-01-01T00:00:00Z'). If only the end date is added, the start date by default will be the end date minus 7 days."
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$startDate,
+        [Parameter(
+            HelpMessage = "The end date of the report, in RFC 3339 standard ('1970-01-01T00:00:00Z'). The default is the current time."
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$endDate,
+        [Parameter(
+            HelpMessage = "The interval selected within the duration of the report. The interval can be per hour, for up to 15 days’ time frame or per day, for between 15 to 30 days' time frame. Submit value in Seconds"
+        )]
+        [ValidateRange(60, 2678400)]
+        [Int32]$interval
+    )
+
+    $filter = Get-ZertoApiFilter -filtertable $PSBoundParameters
+    $uri = "reports/site-journal-size-average{0}" -f $filter
+    Invoke-ZARestRequest -uri $uri
+}
