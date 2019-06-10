a0cc4214ca5ab0e090bc967f0f7d340551bb34e0
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-06-10 09:54:07 -0400

    Add parameter validation

--- ZertoApiWrapper/Public/Get-ZANetworkVpgStat.ps1
+++ ZertoApiWrapper/Public/Get-ZANetworkVpgStat.ps1
@@ -6,14 +6,17 @@ function Get-ZANetworkVpgStat {
             HelpMessage = "The VPG identifier.",
             Mandatory
         )]
+        [ValidateNotNullOrEmpty()]
         [string]$vpgIdentifier,
         [Parameter(
             HelpMessage = "Start date in RFC 3339 standard ('1970-01-01T00:00:00Z'). If the start date is omitted, the default start date is 7 days before the end date."
         )]
+        [ValidateNotNullOrEmpty()]
         [string]$startDate,
         [Parameter(
             HelpMessage = "End date in RFC 3339 standard ('1970-01-01T00:00:00Z'). If the end date is omitted, the default end date is the current time."
         )]
+        [ValidateNotNullOrEmpty()]
         [string]$endDate
     )
 
