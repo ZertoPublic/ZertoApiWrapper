21bc81b0eccb32102dcdaa456164ec9fbb7e293d
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-07-21 18:24:34 -0400

    Add Parameter Tests

--- Tests/Public/Get-ZADatastore.Tests.ps1
+++ Tests/Public/Get-ZADatastore.Tests.ps1
@@ -5,7 +5,34 @@ $global:function = ((Split-Path -leaf $MyInvocation.MyCommand.Path).Split('.'))[
 Describe $global:function -Tag 'Unit', 'Source', 'Built' {
 
     Context "$global:function::Parameter Unit Tests" {
+        it "$global:function should have exactly 14 parameters defined" {
+            (get-command $global:function).Parameters.Count | Should -Be 14
+        }
 
+        $ParameterTestCases = @(
+            @{ParameterName = 'datastoreIdentifier'; Type = 'String'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'clusterIdentifier'; Type = 'String'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'siteIdentifier'; Type = 'String'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+        )
+
+        It "<ParameterName> parameter is of <Type> type" -TestCases $ParameterTestCases {
+            param($ParameterName, $Type, $Mandatory, $Validation)
+            Get-Command $global:function | Should -HaveParameter $ParameterName -Mandatory:$Mandatory -Type $Type
+        }
+
+        It "<ParameterName> parameter has correct validation setting"  -TestCases $ParameterTestCases {
+            param($ParameterName, $Validation)
+            Switch ($Validation) {
+                'NotNullOrEmpty' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateNotNullOrEmpty] }.Count | Should -Be 1
+                }
+
+                default {
+                    $true | should be $false -Because "No Validation Selected. Review test cases"
+                }
+            }
+        }
     }
 
     Context "$global:function::Parameter Functional Tests" {
