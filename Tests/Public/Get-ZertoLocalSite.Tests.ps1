8b0def7c50f8020f395e64cd815bf1741a9f17d2
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-07-23 12:59:23 -0400

    Update Parameter Tests

--- Tests/Public/Get-ZertoLocalSite.Tests.ps1
+++ Tests/Public/Get-ZertoLocalSite.Tests.ps1
@@ -5,7 +5,11 @@ $global:function = ((Split-Path -leaf $MyInvocation.MyCommand.Path).Split('.'))[
 Describe $global:function -Tag 'Unit', 'Source', 'Built' {
 
     Context "$global:function::Parameter Unit Tests" {
-        it "Has a non-mandatory switch parameter for the pairing Statuses" {
+        It "$global:function should have exactly 12 parameters defined" {
+            (Get-Command $global:function).Parameters.Count | Should -Be 12
+        }
+
+        It "Has a non-mandatory switch parameter for the pairing Statuses" {
             Get-Command $global:function | Should -HaveParameter pairingstatuses
             Get-Command $global:function | Should -HaveParameter pairingstatuses -Type switch
         }
