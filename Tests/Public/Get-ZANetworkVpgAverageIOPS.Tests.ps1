e7512020103cd1c8eddc56320d8573433cf6ae4e
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-07-22 20:03:52 -0400

    Add Parameter Tests

--- Tests/Public/Get-ZANetworkVpgAverageIOPS.Tests.ps1
+++ Tests/Public/Get-ZANetworkVpgAverageIOPS.Tests.ps1
@@ -5,7 +5,48 @@ $global:function = ((Split-Path -leaf $MyInvocation.MyCommand.Path).Split('.'))[
 Describe $global:function -Tag 'Unit', 'Source', 'Built' {
 
     Context "$global:function::Parameter Unit Tests" {
+        It "$global:function should have exactly 15 parameters defined" {
+            (Get-Command $global:function).Parameters.Count | Should -Be 15
+        }
 
+        $ParameterTestCases = @(
+            @{ParameterName = 'vpgIdentifier'; Type = 'String'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'startDate'; Type = 'String'; Mandatory = $false; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'endDate'; Type = 'String'; Mandatory = $false; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'interval'; Type = 'Int32'; Mandatory = $false; Validation = 'Range' }
+        )
+
+        It "<ParameterName> parameter is of <Type> type" -TestCases $ParameterTestCases {
+            param($ParameterName, $Type, $Mandatory, $Validation)
+            Get-Command $global:function | Should -HaveParameter $ParameterName -Mandatory:$Mandatory -Type $Type
+        }
+
+        It "<ParameterName> parameter has correct validation setting"  -TestCases $ParameterTestCases {
+            param($ParameterName, $Validation)
+            Switch ($Validation) {
+                'NotNullOrEmpty' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateNotNullOrEmpty] }.Count | Should -Be 1
+                }
+
+                'Range' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateRange] }.Count | Should -Be 1
+                }
+
+                default {
+                    $true | Should be $false -Because "No Validation Selected. Review test cases"
+                }
+            }
+        }
+
+        It "Interval Parameter should have a Min value of 60" {
+            (Get-Command $global:function).Parameters['Interval'].Attributes.Where{ $_ -is [ValidateRange] }.MinRange | Should Be 60
+        }
+
+        It "Interval Parameter should have a Max value of 2678400" {
+            (Get-Command $global:function).Parameters['Interval'].Attributes.Where{ $_ -is [ValidateRange] }.MaxRange | Should Be 2678400
+        }
     }
 
     Context "$global:function::Parameter Functional Tests" {
