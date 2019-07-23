28fabd592c058f4cfe6f2fe9aa27140f71ece754
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-07-22 20:09:03 -0400

    Add Parameter Tests

--- Tests/Public/Get-ZANetworkVpgSummary.Tests.ps1
+++ Tests/Public/Get-ZANetworkVpgSummary.Tests.ps1
@@ -5,7 +5,39 @@ $global:function = ((Split-Path -leaf $MyInvocation.MyCommand.Path).Split('.'))[
 Describe $global:function -Tag 'Unit', 'Source', 'Built' {
 
     Context "$global:function::Parameter Unit Tests" {
+        It "$global:function should have exactly 14 parameters defined" {
+            (Get-Command $global:function).Parameters.Count | Should -Be 14
+        }
 
+        $ParameterTestCases = @(
+            @{ParameterName = 'vpgIdentifier'; Type = 'String'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'startDate'; Type = 'String'; Mandatory = $false; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'endDate'; Type = 'String'; Mandatory = $false; Validation = 'NotNullOrEmpty' }
+        )
+
+        It "<ParameterName> parameter is of <Type> type" -TestCases $ParameterTestCases {
+            param($ParameterName, $Type, $Mandatory, $Validation)
+            Get-Command $global:function | Should -HaveParameter $ParameterName -Mandatory:$Mandatory -Type $Type
+        }
+
+        It "<ParameterName> parameter has correct validation setting"  -TestCases $ParameterTestCases {
+            param($ParameterName, $Validation)
+            Switch ($Validation) {
+                'NotNullOrEmpty' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateNotNullOrEmpty] }.Count | Should -Be 1
+                }
+
+                'Range' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateRange] }.Count | Should -Be 1
+                }
+
+                default {
+                    $true | Should be $false -Because "No Validation Selected. Review test cases"
+                }
+            }
+        }
     }
 
     Context "$global:function::Parameter Functional Tests" {
