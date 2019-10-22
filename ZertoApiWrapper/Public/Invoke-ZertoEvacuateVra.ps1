42b923cef5f9852929269cfaa607318886bd6b79
Wes Carroll <wcarroll@oceanbeachlife.com>
2019-10-22 13:26:10 -0400

    Create EvacuateVra Function

---
+++ ZertoApiWrapper/Public/Invoke-ZertoEvacuateVra.ps1
@@ -0,0 +1,58 @@
+function Invoke-ZertoEvacuateVra {
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = "VraIdentifier")]
+    param (
+        # HostName Option
+        [Parameter(
+            Mandatory,
+            HelpMessage = "Name of the Host to Evacuate",
+            ParameterSetName = "HostName"
+        )]
+        [String]$HostName,
+        # VRA Option
+        [Parameter(
+            Mandatory,
+            HelpMessage = "Name of the VRA to Evacuate",
+            ParameterSetName = "VraName"
+        )]
+        [String]$VraName,
+        # VRAIdentifier Option
+        [Parameter(
+            Mandatory,
+            HelpMessage, "Identifier of the VRA to be evacuated",
+            ParameterSetName = "VraIdentifier",
+            ValueFromPipelineByPropertyName,
+            ValueFromPipeline
+        )]
+        [Alias("VraId", "Identifier")]
+        [String]$VraIdentifier
+    )
+
+    begin {
+
+    }
+
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            "HostName" {
+                $VraName = "Z-VRA-" + $HostName
+                Invoke-ZertoEvacuateVra -VraName $VraName
+            }
+
+            "VraName" {
+                $VraIdentifier = (Get-ZertoVra -vraName $VraName).Identifier
+                Invoke-ZertoEvacuateVra -VraIdentifier $VraIdentifier
+            }
+
+            "VraIdentifier" {
+                $Uri = "vras/{0}/changerecoveryvra/execute" -f $VraIdentifier
+                if ($PSCmdlet.ShouldProcess($VraIdentifier, "Evacuating VRA with Identifier:")) {
+                    Invoke-ZertoRestRequest -Uri $Uri -Method "POST"
+                }
+            }
+        }
+    }
+
+    end {
+
+    }
+}
