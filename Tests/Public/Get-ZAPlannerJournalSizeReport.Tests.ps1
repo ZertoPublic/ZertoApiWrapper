1af70b8c80710ae83fa2edbf92e17d343bf1edf6
Wes Carroll <wcarroll@users.noreply.github.com>
2020-04-02 17:38:35 -0400

    Create Tests

---
+++ Tests/Public/Get-ZAPlannerJournalSizeReport.Tests.ps1
@@ -0,0 +1,57 @@
+#Requires -Modules Pester
+$global:here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+$global:function = ((Split-Path -leaf $MyInvocation.MyCommand.Path).Split('.'))[0]
+
+Describe $global:function -Tag 'Unit', 'Source', 'Built' {
+
+    Context "$global:function::Parameter Unit Tests" {
+        It "$global:function should have exactly 17 parameters defined" {
+            (Get-Command $global:function).Parameters.Count | Should -Be 17
+        }
+
+        $ParameterTestCases = @(
+            @{ParameterName = 'siteIdentifier'; Type = 'String'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'vmIdentifier'; Type = 'String[]'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'recoveryType'; Type = 'String'; Mandatory = $true; Validation = 'Set' }
+            @{ParameterName = 'desiredJournalHistory'; Type = 'String'; Mandatory = $true; Validation = 'Range' }
+            @{ParameterName = 'startDate'; Type = 'String'; Mandatory = $false; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'endDate'; Type = 'String'; Mandatory = $false; Validation = 'NotNullOrEmpty' }
+        )
+
+        It "<ParameterName> parameter is of <Type> type" -TestCases $ParameterTestCases {
+            param($ParameterName, $Type, $Mandatory, $Validation)
+            Get-Command $global:function | Should -HaveParameter $ParameterName -Mandatory:$Mandatory -Type $Type
+        }
+
+        It "<ParameterName> parameter has correct validation setting"  -TestCases $ParameterTestCases {
+            param($ParameterName, $Validation)
+            Switch ($Validation) {
+                'NotNullOrEmpty' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateNotNullOrEmpty] }.Count | Should -Be 1
+                }
+
+                'Range' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateRange] }.Count | Should -Be 1
+                }
+
+                'Set' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateSet] }.Count | Should -Be 1
+                }
+
+                default {
+                    $true | Should be $false -Because "No Validation Selected. Review test cases"
+                }
+            }
+        }
+    }
+
+    Context "$global:function::Parameter Functional Tests" {
+
+    }
+}
+
+Remove-Variable -Name here -Scope Global
+Remove-Variable -Name function -Scope Global
---
+++ Tests/Public/Get-ZAProtectedVm.Tests.ps1
@@ -0,0 +1,41 @@
+#Requires -Modules Pester
+$global:here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+$global:function = ((Split-Path -leaf $MyInvocation.MyCommand.Path).Split('.'))[0]
+
+Describe $global:function -Tag 'Unit', 'Source', 'Built' {
+
+    Context "$global:function::Parameter Unit Tests" {
+        It "$global:function should have exactly 14 parameters defined" {
+            (Get-Command $global:function).Parameters.Count | Should -Be 14
+        }
+
+        $ParameterTestCases = @(
+            @{ParameterName = 'AllVms'; Type = 'Switch'; Mandatory = $false; Validation = $Null }
+            @{ParameterName = 'vmIdentifier'; Type = 'String[]'; Mandatory = $true; Validation = 'NotNullOrEmpty' }
+            @{ParameterName = 'Volumes'; Type = 'Switch'; Mandatory = $false; Validation = $Null }
+        )
+
+        It "<ParameterName> parameter is of <Type> type" -TestCases $ParameterTestCases {
+            param($ParameterName, $Type, $Mandatory, $Validation)
+            Get-Command $global:function | Should -HaveParameter $ParameterName -Mandatory:$Mandatory -Type $Type
+        }
+
+        It "<ParameterName> parameter has correct validation setting"  -TestCases $ParameterTestCases {
+            param($ParameterName, $Validation)
+            Switch ($Validation) {
+                'NotNullOrEmpty' {
+                    $attrs = (Get-Command $global:function).Parameters[$ParameterName].Attributes
+                    $attrs.Where{ $_ -is [ValidateNotNullOrEmpty] }.Count | Should -Be 1
+                }
+
+            }
+        }
+    }
+
+    Context "$global:function::Parameter Functional Tests" {
+
+    }
+}
+
+Remove-Variable -Name here -Scope Global
+Remove-Variable -Name function -Scope Global
